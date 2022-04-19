7c6d17ca18d0b3875500948e1789be137dd7ec4d
mtanaka2613 <103977284+mtanaka2613@users.noreply.github.com>
2022-04-19 13:03:10 -0700

    Update main.py
    This one was creating the time widgets
    -Learned how valuable order is for tkinter as whatever is coded first gets precedence
        i.e. I original put the greeting first before the clock widget and it stayed at the top as opposed to moving down to the middle as I had intended

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import tkinter
 from tkinter import *
 import os
+import time
 
 def greetings():
     greet = "Hi!"
     greeting.config(text=greet)
 
+def timeHour() :
+    string = time.strftime("%H") #how to make hour regular and not military?
+    hour.config(text = string)
+    hour.after(200, timeHour)
+ 
+def timeElse() :
+    string = time.strftime(":%M:%S %p") #%p?
+    other.config(text = string)
+    other.after(200, timeElse)
 
 root = Tk()
 root.title('Mirror')
 #root.configure(background='black')
-    
+
+clock = Label(root) #clock widget
+clock.pack(anchor=NW, fill=X, padx=45)
+clock.configure(background='black')
+hour = Label(root, font = ('Times', 130), bg='black', fg='white') #hour section
+hour.pack(in_=clock, side=LEFT)
+other = Label(root, font = ('Times', 70), bg='black', fg='white') #minutes, miliseconds, and am/pm
+other.pack(in_=clock, side=LEFT, anchor = N, ipady=15)
+
+
 greeting = Label(root, font = ('Times', 300), bg='black', fg='white')
 greeting.pack(anchor=CENTER, fill=X)
 #greeting.place(relx=.5, rely=.5, anchor=CENTER)
 
 greetings()
+timeHour()
+timeElse()
 
 root.attributes("-fullscreen", True)
 root.configure(background='black')
